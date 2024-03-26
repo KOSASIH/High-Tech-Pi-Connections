cbf85b757e3ec0d79b48c11dbbae972721638de6
KOSASIH <kosasihg88@gmail.com>
2024-03-26 09:51:37 +0700

    Create import.py

---
+++ Artificial Intelligence/import.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import tensorflow as tf
+from tensorflow import keras
+import torch
+import torch.nn as nn

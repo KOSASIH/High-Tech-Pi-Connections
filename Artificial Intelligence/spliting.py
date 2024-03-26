916bbc3d2414e2328d165aa78314a8ec7dc7ba85
KOSASIH <kosasihg88@gmail.com>
2024-03-26 09:53:21 +0700

    Create spliting.py

---
+++ Artificial Intelligence/spliting.py
@@ -0,0 +1,8 @@
+# Spliting and scaling datasets sample
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
